c2073bada07c926e4f3c7198f6b827724ce42435
PegVal <107582197+PegVal@users.noreply.github.com>
2023-12-21 21:35:18 +0100

    brandon

--- src/RecordURL.js
+++ src/RecordURL.js
@@ -2,6 +2,8 @@ import { useContext } from "react";
 import { BlogContext } from "./BlogProvider";
 import { useNavigate } from "react-router";
 
+import brandon from "./brandon.png";
+
 
 const RecordURL = () => {
   const { posts } = useContext(BlogContext);
@@ -19,6 +21,42 @@ const RecordURL = () => {
 
   return (
     <>
+      <div class="record-bg">
+        <div className="box-record">
+          <div className="break">
+            <img
+              className="brandon"
+              src={brandon}
+              alt="CSCI E-39"
+            />
+          </div>
+          <div className="header">
+            <div className="title-record">Registered Workout </div>
+          </div>
+          <div className="break-registered-workout">
+            In this Workout, there was {posts.length} timers for a total
+            duration of {totalTiming} seconds.
+            {posts.map((p, index) => (
+              <div
+                key={p.id}
+                index={index}
+                className="workout-container">
+                - "{p.title}" is a {p.type} of {p.duration} seconds with{" "}
+                {p.pause} pause, to be repeated {p.repeat + 1} number of time
+              </div>
+            ))}
+          </div>
+          <div className="btn-record-pos">
+            <button
+              className="button-big"
+              onClick={() => {
+                navigate(-1);
+              }}>
+              Back
+            </button>
+          </div>
+        </div>
+      </div>
       <div className="counter-container">
         <div className="counter-container">
           <div className="panel">
